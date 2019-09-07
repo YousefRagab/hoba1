43357ac672a90064a9f853000a90c1d52dda77ea
YousefRagab <55032542+YousefRagab@users.noreply.github.com>
2019-09-07 21:40:21 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("619978628985258016")
+setInterval(function() {
+channel.send(`yousef gamd of a7 heheheheheh`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
